455dd5c3428d699348b2f6ac4cb7cdabd3bc6970
Darx <mcrivabode@gmail.com>
2022-12-06 10:38:36 -0800

    fix 404 page to properly load

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Matchup from './pages/Matchup';
 import Vote from './pages/Vote';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
       <div className="flex-column justify-center align-center min-100-vh bg-primary">
         <Routes>
-          <Route 
-            path="/" 
-            element={<Home />} 
+          <Route
+            path="/"
+            element={<Home />}
           />
-          <Route 
-            path="/matchup" 
-            element={<Matchup />} 
+          <Route
+            path="/matchup"
+            element={<Matchup />}
           />
-          <Route 
-            path="/matchup/:id" 
-            element={<Vote />} 
+          <Route
+            path="/matchup/:id"
+            element={<Vote />}
+          />
+          <Route
+            path="*"
+            element={<NotFound />}
           />
         </Routes>
       </div>
